a75a02960f8ad729b815e358ed6eb9046d6c3c21
Rozea2 <36906711+Rozea2@users.noreply.github.com>
2023-03-18 09:34:36 +0100

    Update and rename 1.html to 1.php

---
+++ 1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formularz</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<h1>Formularz</h1>
+	<form id="myForm">
+		<label for="data1">Dane 1:</label>
+		<input type="text" id="data1" name="data1"><br><br>
+		<label for="data2">Dane 2:</label>
+		<input type="text" id="data2" name="data2"><br><br>
+		<label for="data3">Dane 3:</label>
+		<input type="text" id="data3" name="data3"><br><br>
+		<input type="submit" value="Wyślij">
+	</form>
+
+	<script src="submit-form.js"></script>
+</body>
+</html>
