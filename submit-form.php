8ed85b7bd7bb931870ce09f393c8d71823e35b32
Rozea2 <36906711+Rozea2@users.noreply.github.com>
2023-03-18 08:42:00 +0100

    Update submit-form.php

--- submit-form.php
+++ submit-form.php
@@ -1,14 +1,39 @@
-<?php
-    if(isset($_POST['data1']) && isset($_POST['data2']) && isset($_POST['data3'])) {
-        $data1 = $_POST['data1'];
-        $data2 = $_POST['data2'];
-        $data3 = $_POST['data3'];
-        $file = "dane.txt";
-        $current = file_get_contents($file);
-        $current .= $data1 . "," . $data2 . "," . $data3 . "\n";
-        file_put_contents($file, $current);
-        echo "Dane zostały zapisane!";
-    } else {
-        echo "Nie udało się zapisać danych.";
-    }
-?>
+<script>
+  const token = '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';
+  const repoName = 'repo';
+  const fileName = 'dane.txt';
+
+  function createFile(content) {
+    const url = `https://api.github.com/repos/${repoName}/contents/${fileName}`;
+    const data = {
+      message: 'Dodano nowy plik',
+      content: btoa(content),
+    };
+    const options = {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    };
+
+    return fetch(url, options)
+      .then((response) => response.json())
+      .catch((error) => console.error('Error:', error));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const n1 = document.getElementById('n1').value;
+    const n2 = document.getElementById('n2').value;
+    const n3 = document.getElementById('n3').value;
+    const content = `${n1}, ${n2}, ${n3}`;
+
+    createFile(content).then(() => {
+      alert('Dane zapisano do pliku na Githubie!');
+      document.getElementById('form').reset();
+    });
+  }
+
+  document.getElementById('form').addEventListener('submit', handleSubmit);
+</script>
