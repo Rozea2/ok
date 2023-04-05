402faa7acdb76da0841d3c021e669dffd4a652ac
Rozea2 <36906711+Rozea2@users.noreply.github.com>
2023-04-05 22:15:38 +0200

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rozszyfrowanie ci¹gu Base64</title>
+	<script>
+		function decodeBase64(encodedString) {
+		  const base64Url = encodedString.replace(/-/g, '+').replace(/_/g, '/');
+		  const base64 = base64Url.replace(/[^A-Za-z0-9+/]/g, '');
+		  const decodedString = atob(base64);
+		  return decodedString;
+		}
+
+		function deszyfruj() {
+		  const encodedString = document.getElementById("ciag-znakow").value;
+		  const wynik = decodeBase64(encodedString);
+		  document.getElementById("wynik-deszyfrowania").innerHTML = wynik;
+		}
+	</script>
+</head>
+<body>
+	<header>
+		<h1>Rozszyfrowanie ci¹gu Base64</h1>
+	</header>
+	
+	<main>
+		<label for="ciag-znakow">Ci¹g znaków do odszyfrowania:</label>
+		<input type="text" id="ciag-znakow" name="ciag-znakow" /><br /><br />
+		<button onclick="deszyfruj()">Rozszyfruj</button><br /><br />
+		<p>Wynik deszyfrowania:</p>
+		<p id="wynik-deszyfrowania"></p>
+	</main>
+</body>
+</html>
